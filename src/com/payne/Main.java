4d2a98e88d8010a14e1fccc3356709bf22fd575e
payne <payne@xqopen.cn>
2018-01-02 16:17:24 +0800

    leetcode p12 Integer to Roman

--- src/com/payne/Main.java
+++ src/com/payne/Main.java
@@ -1,13 +1,22 @@
 package com.payne;
 
+import com.payne.leetCode.P_12_Integer2Roman;
 import com.payne.leetCode.P_13_Roman2Integer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// write your code here
-		P_13_Roman2Integer p = new P_13_Roman2Integer();
+		/*P_13_Roman2Integer p = new P_13_Roman2Integer();
 		int v = p.romanToInt("V");
-		System.out.println(" v = " + v);
+		System.out.println(" v = " + v);*/
+
+		P_12_Integer2Roman p12 = new P_12_Integer2Roman();
+		String s = p12.intToRoman(1900);
+		System.out.println(" s = " + s);
+
+		P_13_Roman2Integer p13 = new P_13_Roman2Integer();
+		int i = p13.romanToInt(s);
+		System.out.println(" i = " + i);
 	}
 }
